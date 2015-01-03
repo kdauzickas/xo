1f87c1e04dba06a9b9198fbcb67525b407858019
Karolis Daužickas <karolis@dauzickas.lt>
2015-01-03 21:31:37 +0200

    Refactor to follow C tutorial more closely

--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+var grid *sdl.Surface
 
 func main() {
-	window, err := sdl.CreateWindow("Tic-tac-toe", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		100, 100, sdl.WINDOW_SHOWN)
+	window, screen := setup()
+	loadAssets()
+
+	grid.Blit(nil, screen, nil)
+	window.UpdateSurface()
+
+	sdl.Delay(5000)
+
+	exit(window)
+}
+
+func setup() (*sdl.Window, *sdl.Surface) {
+	if sdl.Init(sdl.INIT_EVERYTHING) < 0 {
+		panic("Couldn't init SDL")
+	}
+	window, err := sdl.CreateWindow(
+		"Tic-tac-toe",
+		sdl.WINDOWPOS_UNDEFINED,
+		sdl.WINDOWPOS_UNDEFINED,
+		100,
+		100,
+		sdl.WINDOW_SHOWN,
+	)
+
 	if err != nil {
 		panic(err)
 	}
 
-	surface := window.GetSurface()
+	return window, window.GetSurface()
+}
 
-	grid := sdl.LoadBMP("assets/grid.bmp")
+func loadAssets() {
+	grid = sdl.LoadBMP("assets/grid.bmp")
 	if grid == nil {
 		panic("Failed to load grid")
 	}
+}
 
-	grid.Blit(nil, surface, nil)
-
-	window.UpdateSurface()
-
-	sdl.Delay(5000)
+func exit(window *sdl.Window) {
+	grid.Free()
 	window.Destroy()
+	sdl.Quit()
 }
