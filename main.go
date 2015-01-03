08b460876da453f9f3affa8395371231c6a72231
Karolis Daužickas <karolis@dauzickas.lt>
2015-01-03 13:18:10 +0200

    Show grid

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import "github.com/veandco/go-sdl2/sdl"
 
 func main() {
 	window, err := sdl.CreateWindow("Tic-tac-toe", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		600, 600, sdl.WINDOW_SHOWN)
+		100, 100, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
 
 	surface := window.GetSurface()
 
-	rect := sdl.Rect{0, 0, 200, 200}
-	surface.FillRect(&rect, 0xffff0000)
+	grid := sdl.LoadBMP("assets/grid.bmp")
+	if grid == nil {
+		panic("Failed to load grid")
+	}
+
+	grid.Blit(nil, surface, nil)
+
 	window.UpdateSurface()
 
 	sdl.Delay(5000)
